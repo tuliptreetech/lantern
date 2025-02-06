a2ff5d9bc94341752f867dab454c706cd0dd91ee
Ryan Torvik <torvik@tuliptreetech.com>
2025-02-06 18:09:22 -0500

    + add python scripts that will load and reload sketches

---
+++ scripts/load_sketch.py
@@ -0,0 +1,12 @@
+from emerson import Remote
+import logging
+
+logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.WARNING)
+
+if __name__ == "__main__":
+    with Remote(f"http://localhost:10314").connect() as connection:
+        project_name = "arduino-uno-r3"
+        session_id = "arduino-ide-session"
+        connection.run_session(project_name, session_id)
